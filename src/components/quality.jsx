a4d23b1c435569bea7aed5fe59ac4584aff14dfc
Anton Lebedev <sorrint@gmail.com>
2022-07-20 19:20:42 +0800

    add Pagination and PropTypes

--- src/components/quality.jsx
+++ src/components/quality.jsx
@@ -1,10 +1,17 @@
-import React from "react";
-const Quality = ({color, name, _id}) => {
-  return (
-    <span key={_id} className={`badge bg-${color} m-1`}>
-      {name}
-    </span>
-  );
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Quality = ({ color, name, id }) => {
+    return (
+        <span key={id} className={`badge bg-${color} m-1`}>
+            {name}
+        </span>
+    );
+};
+Quality.propTypes = {
+    color: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired
 };
 
 export default Quality;
