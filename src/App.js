4ca97592e8889ad6451eae2d5a81338c04112188
Anton Lebedev <sorrint@gmail.com>
2022-08-02 20:57:16 +0800

    add sort

--- src/App.js
+++ src/App.js
@@ -1,32 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Users from './components/users';
-import api from './api';
 
 function App() {
-    const [users, setUsers] = useState();
-    const handleDelete = (userId) => {
-        setUsers((prevState) => prevState.filter((users) => users._id !== userId));
-    };
-    const handleToggleBookmark = (id) => {
-        const updatedUsers = users.map((user) => {
-            if (user._id === id) {
-                return { ...user, bookmark: !user.bookmark };
-            }
-            return user;
-        });
-        setUsers(updatedUsers);
-    };
-
-    useEffect(() => {
-        api.users.fetchAll().then((data) => setUsers(data));
-    }, []);
-    return (
-        users && (
-            <div>
-                <Users users={users} onDelete={handleDelete} onToggleBookmark={handleToggleBookmark} />
-            </div>
-        )
-    );
+    return <Users />;
 }
 
 export default App;
