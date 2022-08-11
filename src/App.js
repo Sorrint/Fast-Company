846e30b7ee9872ac6631b851ea1da0b4ca9092e8
Anton Lebedev <sorrint@gmail.com>
2022-08-11 23:00:57 +0800

    add routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import Users from './components/users';
+import { Route, Switch } from 'react-router-dom';
+import NavBar from './components/navBar';
+import Main from './layouts/main';
+import Login from './layouts/login';
+import Users from './layouts/users';
 
 function App() {
-    return <Users />;
+    return (
+        <>
+            <NavBar />
+            <Switch>
+                <Route path="/" exact component={Main}></Route>
+                <Route path="/login" component={Login}></Route>
+                <Route path="/users/:userId?" component={Users}></Route>
+            </Switch>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/userPage.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import api from '../api';
+import { useHistory } from 'react-router-dom';
+import QualitiesList from './qualitiesList';
+
+const UserPage = ({ id }) => {
+    const history = useHistory();
+    const [user, setUser] = useState();
+    useEffect(() => {
+        api.users.getById(id).then((data) => setUser(data));
+    }, []);
+    const handleReturn = () => {
+        history.push('/users');
+    };
+    if (user) {
+        return (
+            <>
+                <h1>{user.name}</h1>
+                <h2>Профессия: {user.profession.name}</h2>
+                <h3>completedMeetings: {user.completedMeetings}</h3>
+                <QualitiesList qualities={user.qualities} />
+                <h2>Rate: {user.rate}</h2>
+                <button className="btn btn-outline-primary" onClick={() => handleReturn()}>
+                    Все пользователи
+                </button>
+            </>
+        );
+    } else {
+        return <h1>Loading</h1>;
+    }
+};
+
+export default UserPage;
+
+UserPage.propTypes = {
+    id: PropTypes.string.isRequired
+};
