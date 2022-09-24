7ff677bc5f4c3719d6f03c4bd4998b6c29082637
Anton Lebedev <sorrint@gmail.com>
2022-09-24 19:40:18 +0800

    add API

---
+++ src/app/components/ui/qualities/qualitiesList.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Quality from './quality';
+import { UseQualities } from '../../../hooks/useQualities';
+
+const QualitiesList = ({ qualities }) => {
+    const { isLoading, getQuality } = UseQualities();
+    const qualitiesList = qualities.map((quality) => getQuality(quality));
+    if (!isLoading) {
+        return (
+            <>
+                {qualitiesList.map((quality) => (
+                    <Quality key={quality._id} color={quality.color} name={quality.name} id={quality._id} />
+                ))}
+            </>
+        );
+    } else {
+        return 'Loading';
+    }
+};
+
+export default QualitiesList;
+
+QualitiesList.propTypes = {
+    qualities: PropTypes.array
+};
