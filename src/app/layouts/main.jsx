841730373e80d66105f5702fa468b3815e7a58f4
Anton Lebedev <sorrint@gmail.com>
2023-01-15 11:23:17 +0800

    deploy -v

--- src/app/layouts/main.jsx
+++ src/app/layouts/main.jsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import useMockData from '../utils/mockData';
 
 const Main = () => {
-    const { error, initialize, progress, status } = useMockData();
-    const handleClick = () => {
-        initialize();
-    };
     return (
-        <div className="container mt-5">
-            <h1>Main Page</h1>
-            <h3>Инициализация данных в FireBase</h3>
-            <ul>
-                <li>Status: {status}</li>
-                <li>Progress: {progress}</li>
-                {error && <li>Error:{error}</li>}
-            </ul>
-            <button className="btn btn-primary" onClick={handleClick}>
-                Инициализировать
-            </button>
+        <div className="container mt-5 d-flex flex-column justify-content-center align-items-center text-center">
+            <h1>Добро пожаловать!</h1>
+            <h3>
+                Для просмотра контента можете зарегистрироваться, либо войти по следующим данным:
+                <hr />
+                <div>Логин: newcustomer@example.com</div>
+                <hr />
+                <div>Пароль: Test1234</div>
+            </h3>
         </div>
     );
 };
